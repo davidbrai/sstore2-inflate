589d1e3a506f515205c97a204dd7f32bb78df2b5
David Brailovsky <davidbrai@gmail.com>
2022-06-06 14:15:05 +0200

    poc for reading compressed data with inflate sol and sstore2

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,29 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Contract {}
+import { SSTORE2 } from "solmate/utils/SSTORE2.sol";
+import { InflateLib } from "src/InflateLib.sol";
+
+contract Contract {
+
+    address public pointer;
+    uint256 uncompressedDataLength;
+
+    function read() external view returns (bytes memory) {
+        return SSTORE2.read(pointer);
+    }
+
+    function write(bytes calldata data) external {
+        pointer = SSTORE2.write(data);
+    }
+
+    function writeCompressed(bytes calldata compressedData, uint256 uncompressedDataLength_) external {
+        pointer = SSTORE2.write(compressedData);
+        uncompressedDataLength = uncompressedDataLength_;
+    }
+
+    function readCompressed() external view returns (bytes memory decompressed) {
+        bytes memory data = SSTORE2.read(pointer);
+        (, decompressed) = InflateLib.puff(data, uncompressedDataLength);
+    }
+}
